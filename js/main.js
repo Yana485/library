ec2fe0d0f25d92155b2081fa31be48f9e6d7a756
Yana485 <105270905+Yana485@users.noreply.github.com>
2024-10-13 14:44:22 +0100

    main + phone nav + history

--- js/main.js
+++ js/main.js
@@ -1,33 +1,3 @@
-// // Slideshow
-// let slideIndex = 1;
-// showSlides(slideIndex);
-
-// // Next/previous controls
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
-
-// // Image controls
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
-
-// function showSlides(n) {
-//   let i;
-//   let slides = document.getElementsByClassName("mySlides");
-//   let dots = document.getElementsByClassName("dot");
-//   if (n > slides.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";
-//   dots[slideIndex-1].className += " active";
-// }
-
 var slideIndex = 0;
 carousel();
 
@@ -35,10 +5,11 @@ function carousel() {
   var i;
   var x = document.getElementsByClassName("mySlides");
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+    x[i].style.display = "none";  // Hide all slides
   }
   slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
-  x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 2000); // Change image every 2 seconds
+  if (slideIndex > x.length) {slideIndex = 1}  // Loop back to the first slide
+  x[slideIndex-1].style.display = "block";  // Display current slide
+  x[slideIndex-1].style.transform = "translateY(0)";  // Reset position
+  setTimeout(carousel, 4000);  // Change slide every 2 seconds
 }
