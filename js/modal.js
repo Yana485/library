26cf56764ddde1225a0fb388d0aafde2ab825b31
Yana485 <105270905+Yana485@users.noreply.github.com>
2024-10-15 00:00:48 +0100

    news-template v1

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,17 @@
-// Modal img
 // Get the modal
 var modal = document.getElementById("myModal");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
+
+// Get all images and loop through them to add click event listeners
+var images = document.querySelectorAll('img');
+images.forEach(function(img) {
+  img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  };
+});
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
